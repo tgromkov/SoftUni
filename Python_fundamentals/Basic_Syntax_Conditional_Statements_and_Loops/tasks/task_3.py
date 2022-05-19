9a7d480aaf643286c01befd3e76e87a52324ae31
Toni <tonicobg@gmail.com>
2022-05-19 16:09:06 +0300

    task name: Leonardo DiCaprio Oscars

---
+++ Python_fundamentals/Basic_Syntax_Conditional_Statements_and_Loops/tasks/task_3.py
@@ -0,0 +1,11 @@
+number = int(input())
+
+if number == 88:
+    print("Leo finally won the Oscar! Leo is happy")
+elif number == 86:
+    print("Not even for Wolf of Wall Street?!")
+elif number < 88:
+    if not number == 88 or not number == 86:
+        print("When will you give Leo an Oscar?")
+else:
+    print("Leo got one already!")
