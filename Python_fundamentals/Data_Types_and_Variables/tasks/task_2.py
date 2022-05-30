2f46aff6a75ea3a528b31083f51aa0595fbf7a6e
Toni <tonicobg@gmail.com>
2022-05-30 11:20:46 +0300

    task name: Chars to String

---
+++ Python_fundamentals/Data_Types_and_Variables/tasks/task_2.py
@@ -0,0 +1,10 @@
+def my_function(char_1, char_2, char_3):
+    result = char_1 + char_2 + char_3
+    return result
+
+
+char_1_input = input()
+char_2_input = input()
+char_3_input = input()
+
+print(my_function(char_1_input, char_2_input, char_3_input))
